38c9e2fdb544999cfb1cd0a4aa7464a446fc98b1
Rob Kennedy <63457697+ciderbet@users.noreply.github.com>
2020-08-27 18:58:46 +0100

    category menu

--- pages/user/link/create.js
+++ pages/user/link/create.js
@@ -40,6 +40,32 @@ const Create = () => {
     console.log('POST to server')
   }
 
+  const handleToggle = c => () => {
+    // return the first index or -1
+    const clickedCategory = categories.indexOf(c)
+    const all = [...categories]
+
+    if (clickedCategory === -1) {
+      all.push(c)
+    } else {
+      all.splice(clickedCategory, 1)
+    }
+    console.log('all >> categories', all)
+    setState({ ...state, categories: all, succes: '', error: '' })
+  }
+
+  // show categories checkbox
+  const showCategories = () => {
+    return (
+      loadedCategories &&
+      loadedCategories.map((c, i) => (
+        <li className="list-unstyled" key={c._id}>
+          <input type="checkbox" onChange={handleToggle(c._id)} className="mr-2"/>
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
+    )
+  }
   // link create form
   const submitLinkForm = () => (
     <form onSubmit={handleSubmit}>
@@ -64,12 +90,16 @@ const Create = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-md-4">xxx</div>
+        <div className="col-md-4">
+          <div className="form-group">
+            <label className="text-muted ml-4">Category</label>
+            <ul style={{ maxHeight: '100px', overflowY: 'scroll' }}>{showCategories()}</ul>
+          </div>
+        </div>
         <div className="col-md-8">{submitLinkForm()}
         </div>
       </div>
-      {JSON.stringify(title)}
-      {JSON.stringify(url)}
+      {JSON.stringify(categories)}
     </Layout>
   )
 }
